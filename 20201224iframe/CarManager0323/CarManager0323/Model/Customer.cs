59eabf06fcabf7c10fa74a78f4092414956b8483
limsuwhoan <75109872+limsuwhoan@users.noreply.github.com>
2021-03-30 16:17:56 +0900

    20210330

---
+++ 20201224iframe/CarManager0323/CarManager0323/Model/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManager0323
+{
+    class Customer
+    {
+        string name;
+        string addr;
+        string tel;
+        string email;
+
+        public Customer(string name, string addr, string tel, string email)
+        {
+            this.Name = name;
+            this.Addr = addr;
+            this.Tel = tel;
+            this.Email = email;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public string Addr { get => addr; set => addr = value; }
+        public string Tel { get => tel; set => tel = value; }
+        public string Email { get => email; set => email = value; }
+    }
+}
---
+++ 20201224iframe/CarManager0323/CarManager0323/UI/CustInsForm.cs
@@ -0,0 +1,68 @@
+﻿using CarManager0323.DB;
+using CarManager0323.Handler;
+using CarManager0323.Model;
+using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace CarManager0323.UI
+{
+    partial class CustInsForm : MaterialForm
+    {
+        private DaoOracle oracle;
+        private DealHandler dHandler;
+        public CustInsForm()
+        {
+            InitializeComponent();
+        }
+        public CustInsForm(DaoOracle oracle)
+        {
+            InitializeComponent();
+            this.oracle = oracle;
+            
+        }
+        public CustInsForm(DaoOracle oracle, DealHandler dHandler)
+        {
+            InitializeComponent();
+            this.oracle = oracle;
+            this.dHandler = dHandler;
+        }
+
+        private void CustInsForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void custOK_Click(object sender, EventArgs e)
+        {
+            if (custName.Text == "" || custAddress.Text == "" ||
+                 custTel.Text == "" || custEmail.Text == "") 
+            {
+                MessageBox.Show("누락된 정보가 있습니다.\n" + "올바르게 입력하세요.");
+                return;
+            }
+            List<Deal> list = dHandler.getDealList();
+            try
+            {
+                Customer customer = new Customer(
+                    custName.Text,
+                    custAddress.Text,
+                    custTel.Text,
+                    custEmail.Text);
+                list[0].Customer = customer;
+                oracle.insertCustomer(customer);
+                Close();
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("똑바로 입력하세요");
+            }
+        }
+
+        private void custCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
